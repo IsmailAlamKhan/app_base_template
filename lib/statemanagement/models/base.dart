78d97010d543886fe8199dcec4a64dfdc1c1e32a
Ismail <mdismailalamkhan@gmail.com>
2021-01-15 17:42:32 +0600

    model update

--- lib/statemanagement/models/base.dart
+++ lib/statemanagement/models/base.dart
@@ -1,5 +1,7 @@
-abstract class BaseModel {
+abstract class BaseModel<T> {
   BaseModel();
   BaseModel.fromJson(Map<String, dynamic> json);
   Map<String, dynamic> toJson();
+
+  void copyWith(T newData);
 }
